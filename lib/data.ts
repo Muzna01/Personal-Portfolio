087ab39fcb1b5a2bf96ace63f128e51a6f73bf2e
muznaahmer <muznaahmer@gmail.com>
2024-09-26 21:25:32 +0500

    projects update

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,7 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import GerichtImg from "@/public/web-image (2).png";
+import portfolioImg from "@/public/web-image (1).png";
+import reachImg from "@/public/web-image (3).png";
 
 export const links = [
   {
@@ -36,33 +33,33 @@ export const projectsData = [
     title: "CorpComment",
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-    link: "https://github.com/Muzna01",
+    tags: ["React", "Next.js", "TypeScript", "Tailwind", "Framer Motion"],
+    imageUrl: portfolioImg,
+    link: "https://muzna-portfolio.vercel.app",
   },
- { title: "Word2 Analytics",
+ { title: "Gericht",
+  description:"A resturant app which shows the menu with the prices and ambience of the place ",
+  tags: ["React", "JavaScript", "CSS"],
+  imageUrl: GerichtImg,
+  link: "https://my-app-resturant.vercel.app/", 
+},
+{
+  title: "Yariga",
   description:
-    "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-  tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-  imageUrl: wordanalyticsImg,
-  link: "/ui.earth.com", 
+    "A fully functional dashboard app with CRUD implementation and database management as well. Charts are also used to show analytics",
+  tags: ["MongoDB", "Express", "React", "Node.js"],
+  imageUrl: reachImg,
+  link: "#",
 },
   {
-    title: "rmtDev",
+    title: "REACH Portal",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-    link: "/ui.earth.com",
+    tags: ["HTML", "CSS", "Bootstrap"],
+    imageUrl: reachImg,
+    link: "#",
   },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-    link: "/ui.earth.com",
-  },
+
 ] as const;
 
 export const skillsData = [
