b5b433c67ccf6fa7d93b0ca5c22a5b38473c03c4
muznaahmer <muznaahmer@gmail.com>
2024-09-27 23:19:53 +0500

    mobile view

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
     <html lang="en">
  <body
 
-        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[3000px] pt-28 sm:pt-36 `}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative  pt-28 sm:pt-36 `}
       >
         <Analytics/>
         <div className="absolute top-[-6rem] -z-10 right-[4rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] bg-[#d2935b]"></div>
-        <div className="absolute top-[-1rem] -z-10 left-[12rem] h-[31.25rem] w-[30rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] bg-[#88edcb]"></div>
+        <div className="absolute top-[-1rem] -z-10 left-[12rem] h-[31.25rem] w-[10rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] bg-[#88edcb]"></div>
 
 <Header/>
 
