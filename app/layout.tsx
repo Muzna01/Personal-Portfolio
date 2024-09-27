4e15dec37d1a477d62f351faa3a6c38b70f9b68e
muznaahmer <muznaahmer@gmail.com>
2024-09-27 23:31:50 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       >
         <Analytics/>
         <div className="absolute top-[-6rem] -z-10 right-[4rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] bg-[#d2935b]"></div>
-        <div className="absolute top-[-1rem] -z-10 left-[12rem] h-[31.25rem] w-[10rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] bg-[#88edcb]"></div>
+        <div className="absolute top-[-1rem] -z-10 left-[12rem] h-[31.25rem] w-[7rem] rounded-full blur-[15rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] bg-[#88edcb]"></div>
 
 <Header/>
 
